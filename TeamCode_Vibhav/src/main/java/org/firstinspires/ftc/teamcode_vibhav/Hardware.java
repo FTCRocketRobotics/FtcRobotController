edf355563db94f6d99b2df63668ae4b0f59f3e59
Sky8Rocket <vibhav.javali@gmail.com>
2020-12-19 14:26:01 -0800

    Vibhav: I have made a Hardware class for coding the robot I am working on.

---
+++ TeamCode_Vibhav/src/main/java/org/firstinspires/ftc/teamcode_vibhav/Hardware.java
@@ -0,0 +1,120 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode_vibhav;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Hardware {
+    /*1------------------------------*/
+    /** Public OpMode members. */
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightBack = null;
+    public DcMotor leftArm = null;
+    public Servo leftClaw = null;
+    public Servo rightClaw = null;
+    /*1------------------------------*/
+
+    /*2--------------------------------------------------*/
+    public static final double MID_SERVO =  0.5 ;
+    //public static final double ARM_UP_POWER =  0.45 ;
+    //public static final double ARM_DOWN_POWER = -0.45 ;
+    /*2--------------------------------------------------*/
+
+    /*3--------------------------------------------*/
+    /** local OpMode members. */
+    HardwareMap hwMap =  null;
+    private ElapsedTime period = new ElapsedTime();
+    /*3--------------------------------------------*/
+
+    /*4-----------------*/
+    /** Constructor */
+    public Hardware(){
+
+    }
+    /*4-----------------*/
+
+    /*5--------------------------------------------------------------------*/
+    /** Initialize standard Hardware interfaces */
+    public void init(HardwareMap hw_Map) {
+        /*6-------------------------------*/
+        /** Save reference to Hardware map */
+        hwMap = hw_Map;
+        /*6-------------------------------*/
+
+        /*7--------------------------------------------------------------*/
+        /** Define and Initialize Motors */
+        leftFront = hwMap.get(DcMotor.class, "left_front");
+        rightFront = hwMap.get(DcMotor.class, "right_front");
+        leftBack = hwMap.get(DcMotor.class, "left_back");
+        rightBack = hwMap.get(DcMotor.class, "right_back");
+        leftArm    = hwMap.get(DcMotor.class, "left_arm");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        /*7--------------------------------------------------------------*/
+
+        /*8----------------------------*/
+        /** Set all motors to zero power */
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        leftArm.setPower(0);
+        /*8----------------------------*/
+
+        /*9--------------------------------------------------------------*/
+        /** Set all motors to run without encoders. */
+        /** May want to use RUN_USING_ENCODERS if encoders are installed. */
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        /*9--------------------------------------------------------------*/
+
+        /*10-----------------------------------------------------------*/
+        /** Define and initialize ALL installed servos. */
+        leftClaw  = hwMap.get(Servo.class, "left_hand");
+        rightClaw = hwMap.get(Servo.class, "right_hand");
+        leftClaw.setPosition(MID_SERVO);
+        rightClaw.setPosition(MID_SERVO);
+        /*10-----------------------------------------------------------*/
+    }
+    /*5--------------------------------------------------------------------*/
+ }
+
