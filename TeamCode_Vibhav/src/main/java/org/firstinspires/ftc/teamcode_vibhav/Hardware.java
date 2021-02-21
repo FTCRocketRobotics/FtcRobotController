3afe53cd1226c2d7eaba4a60e024b1298f53854d
Sky8Rocket <vibhav.javali@gmail.com>
2021-02-20 23:00:41 -0800

    I have pulled the required files and I an just commiting my own code

--- TeamCode_Vibhav/src/main/java/org/firstinspires/ftc/teamcode_vibhav/Hardware.java
+++ TeamCode_Vibhav/src/main/java/org/firstinspires/ftc/teamcode_vibhav/Hardware.java
@@ -109,8 +109,8 @@ public class Hardware {
 
         /*10-----------------------------------------------------------*/
         /** Define and initialize ALL installed servos. */
-        leftClaw  = hwMap.get(Servo.class, "left_hand");
-        rightClaw = hwMap.get(Servo.class, "right_hand");
+        leftClaw  = hwMap.get(Servo.class, "left_claw");
+        rightClaw = hwMap.get(Servo.class, "right_claw");
         leftClaw.setPosition(MID_SERVO);
         rightClaw.setPosition(MID_SERVO);
         /*10-----------------------------------------------------------*/
